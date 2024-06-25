f3cbae88379aa50567e5406fd928e46d29910bf6
星月 <528kazusa@gmail.com>
2024-06-25 16:32:00 +0800

    Add BCMath and BitSet.

--- Dockerfile.php
+++ Dockerfile.php
@@ -3,4 +3,4 @@ FROM php:fpm-alpine
 ADD https://github.com/mlocati/docker-php-extension-installer/releases/latest/download/install-php-extensions /usr/bin/
 
 RUN chmod +x /usr/bin/install-php-extensions && \
-    install-php-extensions @composer csv event exif gd http mailparse maxminddb mysqli pdo_dblib pdo_mysql pdo_pgsql pgsql php_trie soap sockets sync xdiff xmlrpc xsl yaml zip zstd
+    install-php-extensions @composer bcmath bitset csv event exif gd http mailparse maxminddb mysqli pdo_dblib pdo_mysql pdo_pgsql pgsql php_trie soap sockets sync xdiff xmlrpc xsl yaml zip zstd
